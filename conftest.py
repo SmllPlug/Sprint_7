51b3e017f87370b9c7d27cea2c2528f53f990411
Kirill Martyushov <martyushov.k.i.m@gmail.com>
2026-02-15 20:22:30 +0300

    Исправление замечания по фикстурам: перенос генерации данных в модуль helpers

--- conftest.py
+++ conftest.py
@@ -4,23 +4,7 @@ import requests
 
 from utils import helpers
 from utils.urls import Urls
-from utils.order_data import BASE_ORDER_DATA
 
-
-@pytest.fixture
-def courier_data():
-    return {
-        'login': helpers.generate_random_string(10),
-        'password': helpers.generate_random_string(10),
-        'firstName': helpers.generate_random_string(10)
-    }
-
-@pytest.fixture
-def courier_without_login():
-    return {
-        'password': helpers.generate_random_string(10),
-        'firstName': helpers.generate_random_string(10)
-    }
 
 @pytest.fixture
 def cleanup_courier():
@@ -32,24 +16,18 @@ def cleanup_courier():
     yield register
     for courier_id in id_to_delete:
         helpers.delete_courier_by_id(courier_id)
+ 
 
 @pytest.fixture
-def courier_without_password():
-    return {
-        'login': helpers.generate_random_string(10),
-        'firstName': helpers.generate_random_string(10)
-    }   
-
-@pytest.fixture
-def created_courier(courier_data):
-    response = requests.post(Urls.CREATE_COURIER, json=courier_data)
-    assert response.status_code == 201
+def created_courier():
+    courier_data = helpers.create_courier_data()
+    helpers.create_courier(courier_data)
     courier_id = helpers.get_courier_id(courier_data['login'], courier_data['password'])
-    assert courier_id is not None
     yield {
         'login': courier_data['login'],
         'password': courier_data['password'],
         'firstName': courier_data['firstName'],
         'courier_id': courier_id
     }
-    helpers.delete_courier_by_id(courier_id)
+    if courier_id:
+        helpers.delete_courier_by_id(courier_id)
--- utils/helpers.py
+++ utils/helpers.py
@@ -13,6 +13,39 @@ def generate_random_string(length):
         random_string = ''.join(random.choice(letters) for i in range(length))
         return random_string
 
+@step('Создание данных для курьера')
+def create_courier_data():
+    login = generate_random_string(10)
+    password = generate_random_string(10)
+    first_name = generate_random_string(10)
+    return {
+          'login': login,
+          'password': password,
+          'firstName': first_name
+    }
+
+@step('Создание курьера без логина')
+def create_courier_without_login():
+    password = generate_random_string(10)
+    first_name = generate_random_string(10)
+    return {
+          'password': password,
+          'firstName': first_name
+    }
+
+@step('Создание курьера без пароля')
+def create_courier_without_password():
+      login = generate_random_string(10)
+      first_name = generate_random_string(10)
+      return {
+              'login': login,
+              'firstName': first_name
+      }
+
+@step('Создание курьера')
+def create_courier(payload):
+      return requests.post(Urls.CREATE_COURIER, json=payload)
+
 @step('Регистрация нового курьера')
 def register_new_courier_and_return_login_password():
     login_pass = []
